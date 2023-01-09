91c2c541d98e66cc865360a3a7bf0d3a5f2f986c
Chahat Kalsi <chahat.ck88@gmail.com>
2023-01-09 07:29:08 +0530

    StudentService tests written with full coverage

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,20 +1,21 @@
 package com.example.demo.student;
 
 import com.example.demo.student.exception.BadRequestException;
-import org.junit.jupiter.api.AfterEach;
+import com.example.demo.student.exception.StudentNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
@@ -46,7 +47,7 @@ class StudentServiceTest {
     }
 
     @Test
-    void addStudent() {
+    void CanAddStudent() {
         // given
         Student student = new Student("ck",
                 "chahat.ck88@gmail.com",
@@ -78,16 +79,45 @@ class StudentServiceTest {
 
         // we need to mock the behaviour of the student repository selectExistsEmail to return true
         // so that the exception is actually thrown
-        given(studentRepository.selectExistsEmail(student.getEmail())).willReturn(true);
+        given(studentRepository.selectExistsEmail(anyString())).willReturn(true);
+        // can pass student.getEmail() instead of actually passing the email
 
         // when
         // then
         assertThatThrownBy(()->studentServiceUnderTest.addStudent(student))
                 .isInstanceOf(BadRequestException.class)
                 .hasMessageContaining("Email "+student.getEmail()+" taken");
+
+
+        // checking that since exception is thrown, studentRepository.save is never called
+        verify(studentRepository, never()).save(any());
     }
 
     @Test
-    void deleteStudent() {
+    void CanDeleteStudent() {
+        // given
+        long id = 8L;
+        given(studentRepository.existsById(anyLong())).willReturn(true);
+
+        // when
+        studentServiceUnderTest.deleteStudent(id);
+
+        // then
+        verify(studentRepository).deleteById(id);
+    }
+
+    @Test
+    void willThrowExceptionWhenDeleteStudentIdNotFound(){
+        // given
+        Long id = 8L;
+        given(studentRepository.existsById(anyLong())).willReturn(false);
+
+        // when
+        // then
+        assertThatThrownBy(()->studentServiceUnderTest.deleteStudent(id))
+                .isInstanceOf(StudentNotFoundException.class)
+                .hasMessageContaining("Student with id " + id + " does not exists");
+
+        verify(studentRepository, never()).deleteById(anyLong());
     }
 }
